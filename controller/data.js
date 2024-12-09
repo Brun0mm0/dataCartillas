8a8d0bdab5cb198812b70164abc92ea4fc1744f1
ProyectosOsssb <bprovenzano@osssb.com.ar>
2024-12-09 11:00:27 -0300

    modificaciones get

--- controller/data.js
+++ controller/data.js
@@ -12,3 +12,39 @@ exports.empresas = (req,res,next) => {
         nombresUnicos
     })
 }
+
+exports.prestadores = (req,res,next) => {
+    
+    const {prestador} = req.query
+    const {data} = dataArr
+
+    const respuesta = data.filter(
+        (prestadores) => prestadores["NOMBRE COMPLETO DEL PRESTADOR"] === prestador 
+    );
+
+    const especialidades = data.map( prestadores => prestadores["ESPECIALIDAD"])
+    const tipo = data.map( prestadores => prestadores["TIPO DE PRESTADOR"])
+    const tipoAtencion = [ ...new Set(tipo) ]
+
+    res.status(200).json({
+        count: respuesta.length,
+        tipoAtencion,
+        especialidades,
+        resp:respuesta[0]
+    })
+}
+
+exports.provincias = (req,res,next) => {
+    
+    const {provincia} = req.query
+    const {data} = dataArr
+
+    const respuesta = data.filter(
+        (provincias) => provincias["PROVINCIA"] === provincia
+    );
+
+    res.status(200).json({
+        count: respuesta.length,
+        respuesta
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@ const data = require('../controller/data')
 /* GET home page. */
 router.route('/')
   .get(data.empresas)
+router.route('/prestadores')
+  .get(data.prestadores)
+router.route('/provincias')
+  .get(data.provincias)
 
 module.exports = router;
